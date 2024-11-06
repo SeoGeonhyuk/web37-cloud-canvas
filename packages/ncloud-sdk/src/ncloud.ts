44337b9021adefc8b40a4ac25bcf4c400dca18f3
sgh4123@naver.com <sgh4123@naver.com>
2024-11-06 21:47:37 +0900

    ✨ Feat: ncloud 객체를 생성할 수 있도록 하는 ncloud.ts 파일 추가
    ISSUES CLOSED: 또한 모듈을 정상적으로 불러올 수 있도록 ncloud-sdk 폴더 아래에 tsconfig.json을 추가함

---
+++ packages/ncloud-sdk/src/ncloud.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface ApiKey {
+    accessKey?: string;
+    secretKey?: string;
+}
+
+export class ncloud {
+    private readonly apiKey: ApiKey;
+    constructor(arg?: ApiKey) {
+        if (arg) {
+            const { accessKey, secretKey } = arg;
+            this.apiKey = {};
+            if (accessKey && secretKey) this.apiKey = arg;
+        } else this.apiKey = {};
+    }
+
+    private getConfigureFilePath() {
+        return path.join(process.cwd(), 'ncloud.config');
+    }
+
+    private readConfigureFile() {
+        const configFilePath = this.getConfigureFilePath();
+        if (!fs.existsSync(configFilePath)) {
+            return {};
+        }
+        const configFileData = JSON.parse(
+            fs.readFileSync(configFilePath, 'utf-8')
+        );
+        return configFileData;
+    }
+
+    keys() {
+        if (this.apiKey.accessKey && this.apiKey.secretKey) {
+            return this.apiKey;
+        }
+        if (
+            process.env['NCLOUD_ACCESS_KEY'] &&
+            process.env['NCLOUD_SECRET_KEY']
+        ) {
+            return {
+                accessKey: process.env['NCLOUD_ACCESS_KEY'],
+                secretKey: process.env['NCLOUD_SECRET_KEY'],
+            };
+        }
+        const config = this.readConfigureFile();
+        return {
+            accessKey: config.accessKey,
+            secretKey: config.secretKey,
+        };
+    }
+}
